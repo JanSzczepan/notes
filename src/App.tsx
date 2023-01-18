1069407ba1b837023ad61369a6f69a970994a838
Jan Szczepan <janszczepan62@gmail.com>
2023-01-18 15:07:35 +0100

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+
 function App() {
    return (
-      <div>
-         <h1>Hi!</h1>
-      </div>
+      <Routes>
+         <Route
+            path='/'
+            element={<h1>Hi</h1>}
+         />
+         <Route
+            path='/new'
+            element={<h1>New</h1>}
+         />
+         <Route path='/:id'>
+            <Route
+               index
+               element={<h1>Show</h1>}
+            />
+            <Route
+               path='edit'
+               element={<h1>Edit</h1>}
+            />
+         </Route>
+         <Route
+            path='*'
+            element={<Navigate to='/' />}
+         />
+      </Routes>
    )
 }
 
